912eb493161a7fd34944a7ff8c188f0421e6b7d2
Komron <komron_0747@mail.ru>
2025-02-17 22:40:35 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header() {
+    return (
+        <header>
+            <h3>REsult School</h3>
+            <span>Временно </span>
+        </header>
+    )
 }
 
-export default App;
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Hello React</h1>
+            </main>
+        </div>
+    )
+}
+
